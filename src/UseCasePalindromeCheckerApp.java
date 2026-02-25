3de1acb4af0663f504b0b9ef07138d63c76c126b
shresth-stack <shresthsehgal11@gmail.com>
2026-02-25 11:02:18 +0530

    UC7

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -6,21 +6,16 @@ public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
 
 
-                // Define the input string
                 String input = "refer";
 
-                // Create a Deque to store characters
                 Deque<Character> deque = new ArrayDeque<>();
 
-                // Add each character to the deque
                 for (char c : input.toCharArray()) {
                     deque.addLast(c);
                 }
 
-                // Flag to track palindrome result
                 boolean isPalindrome = true;
 
-                // Continue comparison while more than one element exists
                 while (deque.size() > 1) {
 
                     char front = deque.removeFirst();
@@ -32,7 +27,6 @@ public class UseCasePalindromeCheckerApp {
                     }
                 }
 
-                // Output result
                 System.out.println("Input : " + input);
                 System.out.println("Is Palindrome? : " + isPalindrome);
             }

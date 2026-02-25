2c5f4238f843787fe6ca35bdd7fb341227fd2b2b
shresth-stack <shresthsehgal11@gmail.com>
2026-02-25 11:11:37 +0530

    UC8

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,27 +1,25 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
-
+import java.util.LinkedList;
 
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
 
 
-                String input = "refer";
+                String input = "level";
 
-                Deque<Character> deque = new ArrayDeque<>();
+                LinkedList<Character> list = new LinkedList<>();
 
                 for (char c : input.toCharArray()) {
-                    deque.addLast(c);
+                    list.add(c);
                 }
 
                 boolean isPalindrome = true;
 
-                while (deque.size() > 1) {
+                while (list.size() > 1) {
 
-                    char front = deque.removeFirst();
-                    char rear = deque.removeLast();
+                    char first = list.removeFirst();
+                    char last = list.removeLast();
 
-                    if (front != rear) {
+                    if (first != last) {
                         isPalindrome = false;
                         break;
                     }
@@ -31,6 +29,7 @@ public class UseCasePalindromeCheckerApp {
                 System.out.println("Is Palindrome? : " + isPalindrome);
             }
         }
+
 
 
 

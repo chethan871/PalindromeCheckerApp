3836bb416bda259200c0967b6bc5ba57c2366827
chethan871 <cj8719@srmist.edu.in>
2026-03-12 09:23:13 +0530

    UC10

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,34 +1,24 @@
-
-
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
 
+        String input = "A man a plan a canal Panama";
 
-                String input = "racecar";
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-                boolean result = check(input, 0, input.length() - 1);
+        boolean isPalindrome = true;
 
-                if (result) {
-                    System.out.println("\"" + input + "\" is a palindrome.");
-                } else {
-                    System.out.println("\"" + input + "\" is NOT a palindrome.");
-                }
-            }
+        for (int i = 0; i < normalized.length() / 2; i++) {
 
-            private static boolean check(String s, int start, int end) {
-
-
-                if (start >= end) {
-                    return true;
-                }
-
-                if (s.charAt(start) != s.charAt(end)) {
-                    return false;
-                }
-
-                return check(s, start + 1, end - 1);
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
             }
         }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
 
 
 
